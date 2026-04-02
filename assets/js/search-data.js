6a1dd96594c6cb671dcd4055e3dc65c610f0b49d
sniang02 <98391638+sniang02@users.noreply.github.com>
2026-04-02 22:42:56 +0000

    Deploying to gh-pages from @ sniang02/sniang02.github.io@ac7a9765da289c092453d752aba33b3813460253 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,18 +72,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
-        
-          title: "displaying beautiful tables with Bootstrap Tables",
-        
-        description: "an example of how to use Bootstrap Tables",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/tables/";
-          
-        },
-      },{id: "post-a-post-with-table-of-contents",
+            },{id: "post-a-post-with-table-of-contents",
         
           title: "a post with table of contents",
         
