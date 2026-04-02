412db054acac83d51d250d221a19763cda7c1226
sniang02 <98391638+sniang02@users.noreply.github.com>
2026-04-02 22:42:38 +0000

    Deploying to gh-pages from @ sniang02/sniang02.github.io@d81ce8f28e139ba8e7086a540321b5c1f6d51654 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -72,18 +72,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-a-post-with-videos",
-        
-          title: "a post with videos",
-        
-        description: "this is what included videos could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/blog/2023/videos/";
-          
-        },
-      },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
+            },{id: "post-displaying-beautiful-tables-with-bootstrap-tables",
         
           title: "displaying beautiful tables with Bootstrap Tables",
         
