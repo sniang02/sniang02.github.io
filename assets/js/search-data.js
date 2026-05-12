24e53841079a6b2719f1b26ce66cc0980e62db7e
sniang02 <98391638+sniang02@users.noreply.github.com>
2026-05-12 13:00:46 +0000

    Deploying to gh-pages from @ sniang02/sniang02.github.io@1f16a891b23b72f40fefa167bda177853f04b710 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,21 +37,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "dropdown-blog",
-              title: "blog",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/blog/";
-              },
-            },{id: "post-the-complete-mathematical-background-for-artificial-intelligence",
+        },{id: "post-the-complete-mathematical-background-for-artificial-intelligence",
         
           title: "The Complete Mathematical Background for Artificial Intelligence",
         
